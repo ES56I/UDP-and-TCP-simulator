a0ce390df45572e6a3636fae7d5ff6891864841f
ES56I <83148876+ES56I@users.noreply.github.com>
2022-03-08 15:46:58 -0500

    Add files via upload

---
+++ TCP_UDP_Simulator/client.py
@@ -0,0 +1,226 @@
+# Import socket module
+from socket import * 
+import sys # In order to terminate the program
+import struct
+import random
+import time
+
+#Code by Evan Imtiaz 
+#-------------------------------------------------------UDP-------------------------------------------------------
+
+#PORT DEFINITION 
+
+serverName = '34.69.60.253'
+
+# serverName = '127.0.0.1'
+
+# -----------------------FUNCTION DEFINITIONS ------------------
+def check_server_response(response):
+    data_len, pcode, entity = struct.unpack_from('!IHH', response)
+    if pcode==555:
+        response = response[8:]
+        print(response.decode())
+        sys.exit()
+    return   
+
+
+#----------------------------------------------------------------
+
+# Assign a port number
+serverPort = 12000
+
+# Bind the socket to server address and server port
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+# TIME OUT SET TO 5 SECONDS
+clientSocket.settimeout(5)
+
+# PACKET CREATION
+data = 'Hello World!!!'
+
+# Padding data
+if len(data) % 4 != 0:
+    Original = len(data)
+    Rem = Original % 4
+    Final = 4 - Rem + Original
+    data = data.ljust(Final, '\0')
+    
+#Defining variables needed for packing    
+data_len = len(data)
+pcode = 0
+entity = 1
+
+#Packing elements requires to be split into two components, header and data.
+Client_packet_1 = struct.pack('!IHH', data_len, pcode, entity)
+Client_Packet_2 = data.encode()
+Client_packet = Client_packet_1 + Client_Packet_2
+
+# Send packet to server
+clientSocket. sendto(Client_packet, (serverName, serverPort))
+
+# Receive packet from server
+Server_Packet, serverAddress = clientSocket.recvfrom(2048)
+
+#VALIDATE THE SERVER PACKET WE RECIEVED
+check_server_response(Server_Packet)
+
+# UNPACK
+Server_Packet_Recieved = struct.unpack('!IHHIIHH', Server_Packet)
+
+print('------------ Starting Stage A  ------------')
+print(f'Recieved packed from server: data_len: {Server_Packet_Recieved[0]} pcode: {Server_Packet_Recieved[1]} entity: {Server_Packet_Recieved[2]} repeat: {Server_Packet_Recieved[3]} udp port: {Server_Packet_Recieved[4]} len: {Server_Packet_Recieved[5]} codeA: {Server_Packet_Recieved[6]}')
+print('------------ End of Stage A  ------------')
+
+#-------------------RE-BINDING PORT--------------------
+
+newport = Server_Packet_Recieved[4]
+serverPort = newport
+
+print('------------ Starting Stage B  ------------')
+
+# PACKET CREATION
+
+Client_padding = 0
+Client_Length = Server_Packet_Recieved[5]
+repeat = Server_Packet_Recieved[3]
+
+#PADDING
+
+if Client_Length % 4 != 0:
+    Rem = Client_Length % 4
+    Client_padding = 4 - Rem
+    
+pcode = Server_Packet_Recieved[6]
+entity = 1
+
+
+if Client_padding != 0:
+    Len_Data = Client_Length + Client_padding
+    data = bytearray(Len_Data * '0'.encode()) 
+else:
+    data = bytearray(Client_Length * '0'.encode())
+    
+data_len = len(data) + 4
+
+Client_Length = len(data)
+repeat_value = 0
+
+
+#INCRIMENT THE REPEAT VALUE UNTIL IT EXCEEDS REPEAT
+
+while repeat_value < repeat:
+    
+    Client_packet_1 = struct.pack('!IHHI', data_len, pcode, entity, repeat_value)
+    
+    Client_packet = Client_packet_1 + data
+    
+    # Send packet to server
+    clientSocket.sendto(Client_packet, (serverName, serverPort))
+    
+    # Receive and unpack packet from server
+    received = False
+    
+    #Try Loop will try to recieve but if it fails it will then sleep for 3 seconds and try again
+    while True:
+        #print("entered the loop")
+        try:
+            Server_Packet, serverAddress = clientSocket.recvfrom(2048)
+            break
+        except:
+            time.sleep(1)
+            clientSocket.sendto(Client_packet, (serverName, serverPort))
+    
+    #print("exited the loop")
+    repeat_value += 1
+    check_server_response(Server_Packet)
+    Server_Packet_Recieved = struct.unpack('!IHHI', Server_Packet)
+    
+    #Print recieved data
+    
+    print(f'Received acknowledgement packet from server: data_len: {Server_Packet_Recieved[0]} pcode: {Server_Packet_Recieved[1]} entity: {Server_Packet_Recieved[2]} acknumber: {Server_Packet_Recieved[3]}')
+
+# Final Packet is recieved and printed to console
+
+Server_Packet, serverAddress = clientSocket.recvfrom(2048)
+Server_Packet_Recieved = struct.unpack('!IHHII', Server_Packet)
+
+tcp_port = Server_Packet_Recieved[3]
+
+print(f'Received final packet: data_len: {Server_Packet_Recieved[0]} pcode: {Server_Packet_Recieved[1]} entity: {Server_Packet_Recieved[2]} tcp_port: {Server_Packet_Recieved[3]} codeB: {Server_Packet_Recieved[4]}')
+print('------------ End of Stage B  ------------')
+
+#----------------------------------TCP---------------------------------------
+
+# Assign a port number
+serverPort = tcp_port
+
+# Bind the socket to server address and server port
+time.sleep(4)
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+print('')
+print('------------ Starting Stage C  ------------')
+print(f'connecting to server at TCP port {tcp_port}')
+
+#RECIEVE PACKETS FROM SERVER
+
+Server_Packet = clientSocket.recv(1024)
+
+Server_Packet_Recieved = struct.unpack('!IHHIIIs', Server_Packet)
+
+decoded_data = Server_Packet_Recieved[6].decode()
+
+print(f'Received packet from server: data_len: {Server_Packet_Recieved[0]}  pcode: {Server_Packet_Recieved[1]} entity: {Server_Packet_Recieved[2]} repeat2: {Server_Packet_Recieved[3]} len2: {Server_Packet_Recieved[4]} codeC: {Server_Packet_Recieved[5]} char: {decoded_data}')
+print('------------ End of Stage C  ------------')
+
+#Sleep timer in place to ensure packages are recieved properly
+
+time.sleep(1)
+repeat2 = Server_Packet_Recieved[3] 
+data_len = Server_Packet_Recieved[4]  
+
+if data_len % 4 != 0:
+    Rem = Server_Packet_Recieved[4] % 4
+    data_len = 4 - Rem + data_len
+    
+data = decoded_data * data_len
+data_encoded = data.encode()
+
+print()
+print('------------ Starting Stage D  ------------')
+print(f'sending {data} to server for {repeat2} times')
+
+entity = 1
+pcode = Server_Packet_Recieved[5]
+repeat_value = 0
+
+
+
+# The while loop recieves a package with each iteration
+
+while repeat_value < repeat2:
+    # print("line 218")
+    Client_packet_1 = struct.pack('!I H H', data_len, pcode, entity)
+    Client_packet = Client_packet_1 + data_encoded
+    #print(Client_Packet)
+    clientSocket.send(Client_packet)
+    repeat_value = repeat_value + 1
+    time.sleep(1)
+    
+#RECIEVE FINAL PACKET
+    
+Server_Packet = clientSocket.recv(1024)
+Server_Packet_Recieved = struct.unpack(f'!IHHI', Server_Packet)
+print(f'Received from server: data_len: {Server_Packet_Recieved[0]} pcode: {Server_Packet_Recieved[1]} entity: {Server_Packet_Recieved[2]} codeD: {Server_Packet_Recieved[3]}')
+
+print()
+print("TESTING SUCCESFUL")
+
+#END CONNECTION 
+
+clientSocket.close() 
+
+
+
---
+++ TCP_UDP_Simulator/server.py
@@ -0,0 +1,200 @@
+# Import socket module
+from socket import * 
+import random
+import struct
+import sys  # In order to terminate the program
+import time
+
+#Code by Evan Imtiaz
+
+#-------------------------------------------------------UDP-------------------------------------------------------
+
+# Assign a port number
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Bind the socket to server address and server port
+serverSocket.bind(('127.0.0.1', serverPort))
+
+#Sets timeout to occur after 3 seconds from last response
+serverSocket.settimeout(3)
+
+#
+while True:
+    client_packet, clientAddress = serverSocket.recvfrom(1024)
+    CHECK_VALUE = client_packet[3] 
+    #print(client_packet[3])
+    length = client_packet[3]
+    client_packet_unpacked = struct.unpack(f'!IHH{length}s', client_packet)
+    
+    
+    compared_message = client_packet_unpacked[3]
+ 
+ 
+    #GENERATE VARIOUS ELEMENTS 
+    client_packet_data = client_packet_unpacked[3].decode()
+    print(f'receiving from the client: data_length: {client_packet_unpacked[0]} code: {client_packet_unpacked[1]} entity: {client_packet_unpacked[2]} data: {client_packet_data}')
+    data_len = struct.calcsize('!IIHH')
+    
+    #VARIABLE DEFINITIONS
+    pcode = 0
+    entity = 2
+    repeat = random.randint(5, 20)
+    length = random.randint(50, 100)
+    udp_port = random.randint(20000, 30000)
+    codeA = random.randint(100, 400)
+    
+    #PACK ELEMENTS
+    server_packet = struct.pack('!IHHIIHH', data_len, pcode, entity, repeat, udp_port, length, codeA)
+    
+    
+    print('------------ Starting Stage A  ------------')
+    print(f'sending to the client: data_length: {data_len} code: {pcode} entity: {entity} repeat: {repeat} udp_port: {udp_port} len: {length} codeA: {codeA}')
+    print(f'SERVER: Server ready on the new UDP port: {udp_port}')
+    print('SERVER:------------ End of Stage A  ------------')
+    print('')
+    
+    # Send packet to client 
+    serverSocket.sendto(server_packet, clientAddress)
+    
+    #Break used to exit the loop
+    break
+
+
+#Setting up new serverPort
+serverPort = udp_port
+
+#Closing connection
+serverSocket.close()
+
+#Opening connection
+Serverside_Socket_The_Second = socket(AF_INET, SOCK_DGRAM)
+
+# Bind the socket to server address and server port
+Serverside_Socket_The_Second.bind(('127.0.0.1', serverPort))
+
+#PADDING
+if length % 4 != 0:
+    Rem = length % 4
+    length = 4 - Rem + length
+
+print('SERVER:------------ Starting Stage B  ------------')
+
+#Variable definitions
+repeat_counter = 0
+
+
+#Generates random ack package and sends to client
+while repeat_counter < repeat:
+    rng_ack = random.randint(1, 100)
+    
+    #Sets up Sending and recieving to Client
+    client_packet, clientAddress = Serverside_Socket_The_Second.recvfrom(2048)
+    client_packet_unpacked = struct.unpack(f'!IHHI{length}s', client_packet)
+    
+    #Organizes elements to send to Client
+    if rng_ack > 50 and client_packet_unpacked[3] == repeat_counter:
+        entity = 2
+        data_len = 4 
+        pcode = client_packet_unpacked[1]
+        
+        #Packs elements to send
+        server_packet = struct.pack('!IHHI', data_len, pcode, entity, repeat_counter)
+        Serverside_Socket_The_Second.sendto(server_packet, clientAddress)
+        repeat_counter+=1
+        
+        print(f'SERVER: received packet_id {client_packet_unpacked[3]} data_len {client_packet_unpacked[0]} pcode: {client_packet_unpacked[1]}')
+
+#Generates Values to pack and send
+data_len = struct.calcsize('!II')
+tcp_port = random.randint(20000, 30000)
+codeB = random.randint(100, 400)
+
+#Packing Elements 
+server_packet = struct.pack('!IHHII', data_len, pcode, entity, tcp_port, codeB)
+
+#Sending Elements
+Serverside_Socket_The_Second.sendto(server_packet, clientAddress)
+
+
+print(f'------------- B2: sending tcp_port {tcp_port} codeB {codeB}')
+print('------------ End of Stage B  ------------')
+
+Serverside_Socket_The_Second.close()  # Terminate the program after sending the corresponding data
+
+#----------------------------------TCP-------------------------------------
+# (AF_INET is used for IPv4 protocols)
+# (SOCK_STREAM is used for TCP)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Defining Port
+serverPort = tcp_port
+
+# Bind the socket to server address and server port
+serverSocket.bind(('127.0.0.1', serverPort))
+
+# listens for 5 seconds
+serverSocket.listen(5)
+
+print('')
+print('------------ Stating Stage C ------------')
+print(f'The server is ready to receive on tcp port: {tcp_port}')
+
+# Definitions
+pcode = codeB
+entity = 2
+repeat2 = random.randint(5, 20)
+len2 = random.randint(50, 100)
+codeC = random.randint(100, 400)
+data = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ').encode()
+data_len = len(data) + struct.calcsize('III')
+
+#Listens for connections
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    server_packet = struct.pack('!IHHIIIs', data_len, pcode, entity, repeat2, len2, codeC, data)
+    print(f'Server Sending to the client: data_length: {data_len} code: {pcode} entity: {entity} repeat2: {repeat2} len2: {len2} codeC: {codeC}')
+    connectionSocket.send(server_packet)
+    break
+
+print('------------ End of Stage C    ------------')
+
+time.sleep(3)
+if len2 % 4 != 0:
+    Rem = len2 % 4 
+    len2 = 4 - Rem + len2
+
+print('------------ Starting Stage D  ------------')
+print('Starting to receive packets from client')
+
+repeat_counter = 0
+
+
+while repeat_counter < repeat2:
+    try:
+        server_packet = connectionSocket.recv(1024)
+        server_packet_unpacked = struct.unpack(f'!IHH{len2}s', server_packet)
+        data = server_packet_unpacked[3].decode()
+        repeat_counter += 1 
+        print(f'repeat_counter = {repeat_counter} data_len: {server_packet_unpacked[0]} pcode: {server_packet_unpacked[1]} entity: {server_packet_unpacked[2]} data: {data}')
+    except: 
+        break
+    
+#DEFINITIONS
+data_len = struct.calcsize('I')
+pcode = codeC
+entity = 2
+codeD = random.randint(100, 400)
+
+#PACKING
+server_packet = struct.pack('!IHHI', data_len, pcode, entity, codeD)
+
+#Sending connections to the server
+connectionSocket.send(server_packet)
+
+#Close connections
+connectionSocket.close()
+serverSocket.close()  
+sys.exit()  # Terminate the program after sending the corresponding data
+
+
